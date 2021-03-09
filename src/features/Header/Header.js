21aa716024ff9b46c40edc822b69d6418227c489
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-03-09 15:41:13 +0000

    Add functinality to button in header to test request for subreddit data

--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { FaReddit } from 'react-icons/fa';
 import { HiOutlineSearch } from 'react-icons/hi';
+import { getSubredditPosts } from '../../api/Reddit';
 
 const Header = () => {
   return (
     <div>
       <FaReddit />
       <input placeholder='Search Here' />
-      <button>
+      <button onClick={() => console.log(getSubredditPosts('nba'))}>
         <HiOutlineSearch />
       </button>
     </div>
