734ddc771d4491dcc50265bfef3aab5005c568c7
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-03-09 12:29:03 +0000

    Create Header folder in features and populate with Header.js and boilerplate. Install react-icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './features/Home/Home';
 import './App.css';
+import Header from './features/Header/Header';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Home />
     </div>
   );
---
+++ src/features/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { faReddit } from 'react-icons/fa';
+const Header = () => {
+  return (
+    <div>
+      <faReddit />
+    </div>
+  );
+};
+
+export default Header;
