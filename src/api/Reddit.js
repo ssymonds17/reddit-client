149c80d8d3f3de6616d4bf44aa70600fac778146
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-03-09 15:38:45 +0000

    Write function to receive posts for a subreddit when the subreddit is provided

---
+++ src/api/Reddit.js
@@ -0,0 +1,8 @@
+export const API_ROOT = 'https://www.reddit.com';
+
+export const getSubredditPosts = async (subreddit) => {
+  const response = await fetch(`${API_ROOT}${subreddit}.json`);
+  const json = await response.json();
+
+  return json.data.children.map((post) => post.data);
+};
